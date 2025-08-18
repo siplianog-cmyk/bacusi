44bb444333bc4544af632c8e606edd63dff54ae0
siplianog-cmyk <siplianog@gmail.com>
2025-08-18 02:10:30 -0300

    tentando o online dnv pela 4 vez

--- public/servidor/server.js
+++ public/servidor/server.js
@@ -5,21 +5,22 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 const players = {};
-setInterval(() => {
-    io.emit('state', players);
-}, 50); // Envia o estado a cada 50ms (20 vezes por segundo)
 
 io.on('connection', (socket) => {
     console.log('Um jogador se conectou:', socket.id);
     players[socket.id] = {
         x: Math.random() * 800,
         y: Math.random() * 600,
-        id: socket.id
+        id: socket.id,
+        espada: false, picareta: false, superpicareta: false, armadura: false,
     };
+    
     socket.emit('currentPlayers', players);
     socket.broadcast.emit('newPlayer', players[socket.id]);
     
@@ -29,12 +30,17 @@ io.on('connection', (socket) => {
         io.emit('playerDisconnected', socket.id);
     });
 
-socket.on('state', (playerData) => {
-    if(players[socket.id]) {
-        players[socket.id] = { ...players[socket.id], ...playerData };
-    }
+    socket.on('state', (playerData) => {
+        if(players[socket.id]) {
+            players[socket.id] = { ...players[socket.id], ...playerData };
+        }
+    });
+
 });
-});
+
+setInterval(() => {
+    io.emit('state', players);
+}, 50); // Envia o estado a cada 50ms (20 vezes por segundo)
 
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
